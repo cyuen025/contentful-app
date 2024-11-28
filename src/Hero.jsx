3caf6d2dd63bc686880966693d07b65e416bec64
Clarice Yuen <cyuen025@gmail.com>
2024-11-28 11:33:43 -0800

    first commit

---
+++ src/Hero.jsx
@@ -0,0 +1,23 @@
+import heroImg from './assets/hero.svg';
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <div className='hero-title'>
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            dignissimos, cupiditate fuga qui rerum nam, hic nihil placeat
+            corrupti at error fugiat, expedita dolore iure ipsum. Doloremque
+            dignissimos nesciunt sapiente.
+          </p>
+        </div>
+        <div className='img-container'>
+          <img src={heroImg} alt='woman and the browser' className='img' />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
